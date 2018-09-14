fbca55b31ff7680a1ae592b399b945248e5c913f
cxl <17621058502@163.com>
2018-09-14 14:09:46 +0800

    add

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -68,6 +68,39 @@ export default {
             });
             return promise;
        },
+    //    catch 的三种表现形式
+       test3(){
+        // this.getJSON("https://api.iviewui.com/v1/ad/list?name=1").then((json)=>{
+        //     console.log('Contents111:'+json);
+        // },(error)=>{
+        //     console.error('出错啦',error)
+        // })
+        // // 方式 一
+        // this.getJSON("/post.json").then((posts)=>{
+        //     // ...
+        // },(error)=>{
+        //     console.log('发生错误一',error)
+        // })
+        // // 方式 二
+        // this.getJSON("/post/json").then((posts)=>{
+
+        // }).then(null,(err)=>{
+        //     console.log('发生错误二',err)
+        // })
+        // // 方式 三
+        // this.getJSON("/post.json").then((posts)=>{
+        //     // ...
+        // }).catch((error)=>{
+        //     console.log('发生错误三！',error);
+        // })
+        this.getJSON("https://api.iviewui.com/v1/ad/list?name=1").then((res)=>{
+            return this.getJSON(res.commentURL);
+        }).then((err2)=>{
+            console.log(err2)
+        }).catch((err)=>{
+            console.log(err)
+        })
+       },
        test4(){
            const p1 = new Promise (function(resolve,reject){
             //    p1 Promise 实例
@@ -89,55 +122,79 @@ export default {
            .catch(error => console.log(error))
        },
        test6(){
-           new Promise ((resolve,reject)=>{
-            //    resolve(1);
-               return resolve(1);
-               console.log(2);
-           }).then((r)=>{
-               console.log(r);
-           })
+            new Promise ((resolve,reject)=>{
+                //    resolve(1);
+                return resolve(1);
+                console.log(2);
+            }).then((r)=>{
+                console.log(r);
+            })
        },
-       /**
-        * promise 实例具有 then 方法，
-        * then 方法是定义在原型对象 promise.prototype 上的
-        * 作用是 为Promise实例添加状态改变时的回调函数
-        * then 方法第一个参数是 resolve状态的回调函数
-        * 第二个参数是reject状态的回调函数（可选）
-        * then 方法返回的是一个新的promise 实例（不是原来那个Promise实例）
-        * 因此可以采用链式写法，即then的方法后面在调用另一个then方法
-        */ 
+       test7(){
+            const promise = new Promise (function(resolve,reject){
+                throw new Error ('test')
+            });
+            promise.catch(function(error){
+                console.log(error)
+            })
+            // 写法一
+            const promise1 = new Promise ((resolve,reject)=>{
+                try{
+                    throw new Error ('test1111111')
+                }catch(e){
+                    console.log(e);
+                }
+            })
+            promise1.catch((err)=>{
+                console.log(err +'666') // 加不上字符串
+            })
+            // 写法二
+            const promise2 = new Promise((resolve,reject)=>{
+                reject(new Error('test2'))
+            });
+            promise2.catch((err)=>{
+                console.log(err+'2222222')
+            })
+        },
+        // 如果Promise状态已经变成resolve，再抛出错误是无效的
+        test8(){
+            const promise = new Promise((resolve,reject)=>{
+                resolve('ok')
+                throw new Error ('test')
+            });
+            promise
+                .then((res)=>{console.log(res)})
+                .catch((error)=>{console.log(error)})
+        },
+        test9(){
+            const promise = new Promise ((resolve,reject)=>{
+                resolve('ok')
+                setTimeout(()=>{ throw new Error('test')},0)
+            })
+            promise.then(function(value){console.log(value)})
 
+        },
+        // finally 实现的方式
+        test10(){
+            Promise.prototype.finally = callback =>{
+                let p = this.constructor;
+                return this.then(
+                    value => P.resolve(callback()).then(()=>value),
+                    reason => p.resolve(callback()).then(()=>{throw reason})
+                )
+            }
+        }
+       
     },
     created(){
         // this.test1();
         // this.test2();
+        // this.test3();
         // this.test5();
         // this.test6();
-        this.getJSON("https://api.iviewui.com/v1/ad/list?name=1").then((json)=>{
-            console.log('Contents:'+json);
-        },(error)=>{
-            console.error('出错啦',error)
-        })
-        // 方式一
-        this.getJSON("/post.json").then((posts)=>{
-            // ...
-        },(error)=>{
-            console.log('发生错误一',error)
-        })
-        // 方式 二
-        this.getJSON("/post/json").then((posts)=>{
-
-        }).then(null,(err)=>{
-            console.log('发生错误二',err)
-        })
-        // 方式 三
-        this.getJSON("/post.json").then((posts)=>{
-            // ...
-        }).catch((error)=>{
-            console.log('发生错误三！',error);
-        })
-
-
+        // this.test7();
+        // this.test8();
+        this.test9();
     }
 }
 </script>
