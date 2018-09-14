1356b1d0588a204a52661ccef6be99dfa205e651
cxl <17621058502@163.com>
2018-09-14 18:22:41 +0800

    整理目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/pages/index'
+import test1 from '@/test/test1'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-  
     {
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: '/test1',
+      name: 'test1',
+      component: test1
     }
   ]
 })
